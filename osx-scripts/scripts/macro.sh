9b6ec2b952aa8c573ef4d70803b4223f98764201
Stefan Schneider-Kennedy <code@stefansk.name>
2023-06-05 09:53:17 +1000

    Add OSX macro script

---
+++ osx-scripts/scripts/macro.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+yabai -m window --focus west
+# cliclick t:q kd:ctrl t:w ku:ctrl kd:ctrl t:l ku:ctrl kp:arrow-up kp:return
+cliclick t:q kd:ctrl t:w ku:ctrl kp:arrow-up kp:return
+yabai -m window --focus east
