892a2ce2a5af42593f4d99d125cc453af626a6cd
Stefan Schneider-Kennedy <code@stefansk.name>
2023-07-02 20:43:22 +1000

    Adjustments to OSX scripts

--- osx-scripts/scripts/focus-first-window-in-space.sh
+++ osx-scripts/scripts/focus-first-window-in-space.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if yabai -m query --windows --space | jq -er 'map(select(."is-floating" == false)) | map(select(.focused == 1)) | length == 0' >/dev/null; then
+if yabai -m query --windows --space | jq -er 'map(select(."is-floating" == false)) | map(select(."has-focus" == true)) | length == 0' >/dev/null; then
     # Is-floating to not pick up teams windows
     id=`yabai -m query --windows --space | jq -r 'map(select(."is-floating" == false and .frame.h != 0)) | .[0].id'`
     [ $id != null ] && yabai -m window --focus $id
--- osx-scripts/scripts/macro.sh
+++ osx-scripts/scripts/macro.sh
@@ -2,5 +2,6 @@
 
 yabai -m window --focus west
 # cliclick t:q kd:ctrl t:w ku:ctrl kd:ctrl t:l ku:ctrl kp:arrow-up kp:return
-cliclick t:q kd:ctrl t:w ku:ctrl kp:arrow-up kp:return
+# cliclick t:q kd:ctrl t:w ku:ctrl kp:arrow-up kp:return
+cliclick kp:arrow-up kp:return
 yabai -m window --focus east
