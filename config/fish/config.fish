f8589b797ccb844caf8fbddce95dc89a7bfa94fa
Stefan Schneider-Kennedy <code@stefansk.name>
2023-12-09 12:38:58 +1100

    Make fish work a bit better on OSX

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,22 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set EDITOR '/usr/bin/nvim'
-    set GIT_EDITOR '/usr/bin/nvim'
-    set GOPATH "$HOME/.cache/go"
+    set -x EDITOR 'nvim'
+    set -x GIT_EDITOR 'nvim'
     fish_add_path -a ~/bin
     fish_add_path -a ~/bin/mine
-    fish_add_path -a ~/Programming/small-utilities
-    fish_add_path -a $GOPATH/bin
     alias vim='nvim'
     alias gg='git status'
+    alias dc='docker-compose'
     alias kssh='kitty +kitten ssh'
     alias pvim='poetry run nvim'
+    set uname $(uname)
+    switch $uname
+        case Darwin
+            fish_add_path -a ~/software/small-utilities
+            set -U GIT_EDITOR "$HOME/bin/git_commit_editor"
+        case Linux
+            set -x GOPATH "$HOME/.cache/go"
+            fish_add_path -a ~/Programming/small-utilities
+            fish_add_path -a $GOPATH/bin
+    end
 end
