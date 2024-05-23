6d10575a5a7e6fdd9c7db3756ce970556bb95d19
Stefan Schneider-Kennedy <code@stefansk.name>
2024-05-23 10:28:51 +1000

    fix: Get todo.txt tracking: highlighted again

--- config/nvim/syntax/todo.lua
+++ config/nvim/syntax/todo.lua
@@ -1,8 +1,8 @@
--- The todo.txt-vim syntax highlighting only allows certain match groups
--- within it. So I just piggy back on one of those.
-vim.cmd.syntax('match OverDueDate \'tracking:\\d\\+\'')
-vim.cmd.highlight('default link OverDueDate Character')
+-- Make the tracking text red
+vim.cmd.syntax "match TodoTrackingTime 'tracking:\\d\\+'"
+vim.cmd.highlight 'TodoTrackingTime ctermfg=01'
 
-vim.cmd.highlight('link TodoPriorityA Normal')
-vim.cmd.highlight('link TodoPriorityB Normal')
-vim.cmd.highlight('link TodoPriorityC Normal')
+-- Stop some things from being highlighted
+vim.cmd.highlight 'link TodoPriorityA Normal'
+vim.cmd.highlight 'link TodoPriorityB Normal'
+vim.cmd.highlight 'link TodoPriorityC Normal'
