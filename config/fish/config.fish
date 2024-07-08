dce1b248131be14d464c26074988e2453991a65b
Stefan Schneider-Kennedy <code@stefansk.name>
2024-07-08 10:02:36 +1000

    Include some more OSX fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,10 +3,11 @@ if status is-interactive
     set -x EDITOR 'nvim'
     set -x GIT_EDITOR 'nvim'
     set GOPATH "$HOME/.cache/go"
-    set -x SSH_AUTH_SOCK "/var/run/user/1000/ssh-agent.sock"
     set -U fish_greeting
     fish_add_path -a ~/bin
     fish_add_path -a ~/bin/mine
+    fish_add_path -a ~/go/bin/
+    fish_add_path -a ~/.local/bin
     alias vim='nvim'
     alias gg='git status'
     alias dc='docker-compose'
@@ -16,9 +17,12 @@ if status is-interactive
     switch $uname
         case Darwin
             fish_add_path -a ~/software/small-utilities
+            pyenv init - | source
             set -U GIT_EDITOR "$HOME/bin/git_commit_editor"
+            set -x SSH_AUTH_SOCK "$HOME/.local/state/ssh-agent.sock"
         case Linux
             fish_add_path -a ~/Programming/small-utilities
             fish_add_path -a $GOPATH/bin
+            set -x SSH_AUTH_SOCK "/var/run/user/1000/ssh-agent.sock"
     end
 end
