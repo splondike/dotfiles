46d7fad04ae3224a90a4512ed32e18ba5f00d198
Stefan Schneider-Kennedy <code@stefansk.name>
2024-07-17 14:31:18 +1000

    NVim: Include words from open buffers in autocomplete

--- config/nvim/lua/thirdparty/plugins/cmp.lua
+++ config/nvim/lua/thirdparty/plugins/cmp.lua
@@ -109,7 +109,14 @@ return {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
-          { name = 'buffer' },
+          {
+            name = 'buffer',
+            option = {
+              get_bufnrs = function()
+                return vim.api.nvim_list_bufs()
+              end,
+            },
+          },
         },
       }
     end,
