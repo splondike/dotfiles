f0dca0fb065100d9dc63ebc7420203c0226758d2
Stefan Schneider-Kennedy <code@stefansk.name>
2024-07-31 08:41:04 +1000

    Enable SnipMate snippets in NVim

--- config/nvim/lua/thirdparty/plugins/cmp.lua
+++ config/nvim/lua/thirdparty/plugins/cmp.lua
@@ -41,6 +41,7 @@ return {
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
+      require('luasnip.loaders.from_snipmate').lazy_load()
 
       cmp.setup {
         snippet = {
