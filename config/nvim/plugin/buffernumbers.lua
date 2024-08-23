d2da8b5e81873df8f8d47e13559f1d54e9931cb8
Stefan Schneider-Kennedy <code@stefansk.name>
2024-08-23 12:30:47 +1000

    Nvim buffer numbers updates

--- config/nvim/plugin/buffernumbers.lua
+++ config/nvim/plugin/buffernumbers.lua
@@ -1,15 +1,32 @@
+-- C-m [1-9] will save the current buffer to a bookmark. C-[1-9] restores it. C-0 jumps back to the last buffer that was open before you started using C-[1-9]
 local bookmarks = {}
-local swap_buffer = function(num)
+local last_buffer_jumped_to = nil
+local first_buffer_jumped_from = nil
+local function swap_buffer(num)
   if bookmarks[num] then
+    local curr_buffer = vim.fn.bufnr()
+    if curr_buffer ~= last_buffer_jumped_to then
+      first_buffer_jumped_from = curr_buffer
+    end
+
     vim.cmd.buffer(bookmarks[num])
+    last_buffer_jumped_to = bookmarks[num]
   end
 end
 
-local set_buffer = function(num)
+local function swap_buffer_to_pre_jump()
+  if first_buffer_jumped_from then
+    vim.cmd.buffer(first_buffer_jumped_from)
+  end
+end
+
+local function set_buffer(num)
   bookmarks[num] = vim.fn.bufnr()
 end
 
-for i = 1, 5 do
+vim.keymap.set('n', '<C-0>', swap_buffer_to_pre_jump)
+
+for i = 1, 9 do
   vim.keymap.set('n', '<C-' .. i .. '>', function()
     swap_buffer(i)
   end)
