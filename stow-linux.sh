fc34a88cc5e44ce6cb9ab8f43fdbe99aec2a7167
Stefan Schneider-Kennedy <code@stefansk.name>
2024-08-28 22:29:33 +1000

    CalDAV and CardDAV client config

--- stow-linux.sh
+++ stow-linux.sh
@@ -3,5 +3,5 @@
 cd $(dirname $(readlink -f $0))
 . ./stow-home-general.sh
 
-packages=(fish aerc git kitty nvim nvim-local rofi sway waybar zellij makenote notmuch w3m)
+packages=(fish aerc git kitty nvim nvim-local rofi sway waybar zellij makenote notmuch w3m vdirsyncer khal khard)
 do_stow "${packages[@]}"
