034f2755a49dfb7a6d4f0ae7b70a1b7890c56179
Stefan Schneider-Kennedy <code@stefansk.name>
2024-09-16 11:32:22 +1000

    Re-implement desired todo.txt highlighting

--- home-public/nvim/dot-config/nvim/syntax/todo.lua
+++ home-public/nvim/dot-config/nvim/syntax/todo.lua
@@ -1,8 +1,11 @@
--- Stop some things from being highlighted
-vim.cmd.highlight 'clear TodoPriorityA'
-vim.cmd.highlight 'clear TodoPriorityB'
-vim.cmd.highlight 'clear TodoPriorityC'
+-- Early exit the highlighting rules from the plugin's syntax highlighter, I can't work out how to clear it after the fact
+vim.b.current_syntax = 'todo-custom'
 
--- Make the tracking text red
 vim.cmd.syntax "match TodoTrackingTime 'tracking:\\d\\+'"
 vim.cmd.highlight 'TodoTrackingTime ctermfg=01'
+vim.cmd.syntax "match TodoDue 'due:\\d\\{4\\}-\\d\\{2\\}-\\d\\{2\\}'"
+vim.cmd.highlight 'TodoDue ctermfg=03'
+vim.cmd.syntax "match TodoProject ' @[a-z-]\\+'"
+vim.cmd.highlight 'TodoProject ctermfg=06'
+vim.cmd.syntax "match TodoDone '^x .\\+'"
+vim.cmd.highlight 'TodoDone ctermfg=08'
