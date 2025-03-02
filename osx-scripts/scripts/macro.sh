50a1e2b1b6578daab44d1b2a3f86bdc287252ce8
Stefan Schneider-Kennedy <code@stefansk.name>
2025-03-03 10:24:38 +1100

    feat: Make OSX macro parameterised

--- osx-scripts/scripts/macro.sh
+++ osx-scripts/scripts/macro.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
-# In yabai
-# yabai -m window --focus west
-# cliclick t:q kd:ctrl t:w ku:ctrl kd:ctrl t:l ku:ctrl kp:arrow-up kp:return
-# cliclick t:q kd:ctrl t:w ku:ctrl kp:arrow-up kp:return
-# yabai -m window --focus east
+macro_mode_file="$HOME/.cache/osx-macro-mode"
+if [ -e "$macro_mode_file" ];then
+    option=$(cat "$macro_mode_file")
+else
+    option=default
+fi
+
+if [ "$option" = "default" ];then
+    # Ctrl-l up enter
+    printf '\x0c\x1b\x5b\x41\x0d' | kitty-send-text
+elif [ "$option" = "swapwin" ];then
+    # Swap to last focussed window
+    aerospace focus-back-and-forth
+else
+    echo "Unknown option: $option"
+fi
 
 # In Zellij
 # cliclick kd:alt kp:arrow-right ku:alt
 # cliclick kd:ctrl t:l ku:ctrl kp:arrow-up kp:return
 # cliclick kd:alt kp:arrow-left ku:alt
 
-# Pure kitty
-# See GNU showkey -a for keycodes. Just use a Linux VM to check things
-
-# Ctrl-c (03) Ctrl-l (0c) up (1b 5b 41) enter (0d)
-# printf '\x03\x0c\x1b\x5b\x41\x0d' | kitty-send-text
-# Ctrl-l up enter
-printf '\x0c\x1b\x5b\x41\x0d' | kitty-send-text
-
-# Mermaid
-# pbcopy < ~/blah.erd
-# yabai -m window --focus west
+# # Mermaid
+# pbcopy < ~/blah.mermaid
+# aerospace focus left
 # cliclick kd:cmd t:a t:v ku:cmd
-# yabai -m window --focus east
+# aerospace focus right
