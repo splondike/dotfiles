6958097cd807b2d0042339017baec588022a32f7
Stefan Schneider-Kennedy <code@stefansk.name>
2025-11-04 11:53:13 +1100

    feat: Neovim spellcheck helper binding

--- home-public/nvim/dot-config/nvim/ftplugin/markdown.lua
+++ home-public/nvim/dot-config/nvim/ftplugin/markdown.lua
@@ -1,1 +1,12 @@
 vim.wo.foldmethod = 'marker'
+
+local spell_check = function()
+  vim.wo.spell = true
+  vim.cmd.normal ']si'
+  vim.cmd.startinsert()
+  local key = vim.api.nvim_replace_termcodes('<C-x><C-s>', true, true, true)
+  vim.api.nvim_feedkeys(key, 'i', false)
+end
+
+vim.keymap.set('n', '<C-b>', spell_check, { buffer = true })
+vim.keymap.set('i', '<C-b>', spell_check, { buffer = true })
