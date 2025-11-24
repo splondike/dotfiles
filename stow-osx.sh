8ff03bc228225f4abc07295a8fa3b3fa58aa675c
Stefan Schneider-Kennedy <code@stefansk.name>
2025-11-25 10:35:37 +1100

    feat: Add k9s config

--- stow-osx.sh
+++ stow-osx.sh
@@ -3,5 +3,5 @@
 cd $(dirname $(readlink -f $0))
 . ./stow-home-general.sh
 
-packages=(fish git tig kitty nvim nvim-local zellij makenote w3m skhd karabiner aerospace nushell visidata)
+packages=(fish git tig kitty nvim nvim-local zellij makenote w3m skhd aerospace nushell visidata k9s)
 do_stow "${packages[@]}"
