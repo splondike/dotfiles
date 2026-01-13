e412e258892927eaa23a1c3d18a59c3bf4e7181f
Stefan Schneider-Kennedy <code@stefansk.name>
2026-01-13 17:50:06 +1100

    feat: Include execution time and status code in fish prompt

--- home-public/fish/dot-config/fish/functions/fish_prompt.fish
+++ home-public/fish/dot-config/fish/functions/fish_prompt.fish
@@ -1,8 +1,5 @@
 function fish_prompt
-    # This is a simple prompt. It looks like
-    # alfa@nobby /path/to/dir $
-    # with the path shortened and colored
-    # and a "#" instead of a "$" when run as root.
+    set -l last_returncode $status
     set -l symbol '$ '
     set -l color $fish_color_cwd
     if fish_is_root_user
@@ -13,6 +10,14 @@ function fish_prompt
 
     set_color $color
     echo -n "$(date +%H:%M:%S) "
+    if test $CMD_DURATION -gt 1000
+        echo -n $CMD_DURATION"ms "
+    end
+    if test $last_returncode -ne 0
+        set_color red
+        echo -n "$last_returncode "
+        set_color $color
+    end
     echo -n (prompt_pwd)
     echo -n $symbol
     set_color normal
