f00c8ace5e8669dc1c195945bca46043b968608b
Stefan Schneider-Kennedy <code@stefansk.name>
2026-07-12 15:57:15 +1000

    sway: Update macro command

--- home-public/sway/dot-config/sway/scripts/macro.sh
+++ home-public/sway/dot-config/sway/scripts/macro.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Ctrl-c
+printf '\x03' | ~/.config/kitty/scripts/kitty-send-text $@
+sleep 0.5
 # Ctrl-l, go up, enter
 printf '\x0c\x1b\x5b\x41\x0d' | ~/.config/kitty/scripts/kitty-send-text $@
